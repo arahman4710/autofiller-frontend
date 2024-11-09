e23957cacc49fe170dada8f80af7ae20e49941d0
Anik Rahman <arahman4710@gmail.com>
2024-11-08 22:30:40 -0500

    update

--- apps/main-app/src/types/auth.d.ts
+++ apps/main-app/src/types/auth.d.ts
@@ -2,16 +2,9 @@ import 'next-auth'
 
 declare module 'next-auth' {
   // eslint-disable-next-line
-  interface Business {
-    id: string
-    name: string
-  }
-
-  // eslint-disable-next-line
   interface User {
     id: string
     token: string
-    business: Business
   }
 
   // eslint-disable-next-line

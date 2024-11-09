8ca78852d5172f44a186586b85a8ea5c05c17520
Anik Rahman <arahman4710@gmail.com>
2024-11-08 20:19:25 -0500

    Update unused code

--- apps/main-app/src/app/(main)/chat/page.tsx
+++ apps/main-app/src/app/(main)/chat/page.tsx
@@ -1,8 +1,6 @@
-import { Button } from '@rag/ui/Button'
 import { MainShell } from '@rag/ui/MainShell'
 import { Metadata } from 'next'
 import { StartChat } from './components/StartChat'
-import { UploadFile } from './components/UploadFile'
 
 export const metadata: Metadata = {
   title: 'Dashboard',

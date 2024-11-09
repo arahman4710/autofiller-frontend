eebfde1d964ea24bd65aed2ef3888766e47f485f
Anik Rahman <arahman4710@gmail.com>
2024-11-08 22:12:19 -0500

    Update

--- apps/main-app/src/components/Logo.tsx
+++ apps/main-app/src/components/Logo.tsx
@@ -1,12 +1,6 @@
 'use client'
 
-import { CanyonLogoFull, CanyonLogoProFull } from '@rag/ui/CanyonLogo'
-
-import { useCurrentUser } from '@/hooks/useCurrentUser'
-
 export const Logo = () => {
-  const { isAdvisoryOrgAccount, isPaidPlan } = useCurrentUser()
-
   return (
     <div>
       RetailMind

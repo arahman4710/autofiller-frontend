38416e8df9209ef4a67c3962b2d5c00a85f190e4
Anik Rahman <arahman4710@gmail.com>
2024-11-19 16:36:57 -0500

    Google oauth provider

--- apps/main-app/src/app/provider/index.tsx
+++ apps/main-app/src/app/provider/index.tsx
@@ -9,6 +9,8 @@ import { AuthProvider } from '@/app/provider/Auth'
 import { GlobalDialogsProvider } from '@/app/provider/GlobalDialogsProvider'
 import { PostHogProvider } from '@/app/provider/Posthog'
 import { rollbarConfig } from '@/lib/rollbar'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 
 // MSW is not properly supported for TurboPack (or NextJS v14+ in general)
 if (!process.env.NEXT_PUBLIC_TURBO_ENABLED && process.env.NODE_ENV === 'development') {
@@ -24,18 +26,20 @@ interface IProviders {
 
 export const Providers = ({ children, token }: IProviders) => {
   return (
-    <Provider config={rollbarConfig}>
-      <ErrorBoundary>
-        <AuthProvider>
-          <PostHogProvider>
-            <TooltipProvider delayDuration={400}>
-              <ApolloProvider token={token}>
-                <GlobalDialogsProvider>{children}</GlobalDialogsProvider>
-              </ApolloProvider>
-            </TooltipProvider>
-          </PostHogProvider>
-        </AuthProvider>
-      </ErrorBoundary>
-    </Provider>
+    <GoogleOAuthProvider clientId={process.env.GOOGLE_DRIVE_CLIENT_ID || ''}>
+      <Provider config={rollbarConfig}>
+        <ErrorBoundary>
+          <AuthProvider>
+            <PostHogProvider>
+              <TooltipProvider delayDuration={400}>
+                <ApolloProvider token={token}>
+                  <GlobalDialogsProvider>{children}</GlobalDialogsProvider>
+                </ApolloProvider>
+              </TooltipProvider>
+            </PostHogProvider>
+          </AuthProvider>
+        </ErrorBoundary>
+      </Provider>
+    </GoogleOAuthProvider>
   )
 }
--- apps/main-app/src/components/toolbars/DocumentsToolbar.tsx
+++ apps/main-app/src/components/toolbars/DocumentsToolbar.tsx
@@ -4,16 +4,33 @@ import { useCallback, useState } from 'react'
 
 import { Button } from '@rag/ui/Button'
 import { Toolbar } from '@rag/ui/Toolbar'
-import { PlusCircle } from '@phosphor-icons/react'
+import { GoogleDriveLogo, PlusCircle } from '@phosphor-icons/react'
 
 import { UploadDocumentDialog } from '@/components/dialogs/UploadDocumentDialog'
+import { GoogleLogin } from '@react-oauth/google';
+import { googleLogout, useGoogleLogin } from '@react-oauth/google';
+
+
 
 export const DocumentsToolbar = () => {
   const [uploadDocumentDialogOpen, setuploadDocumentDialogOpen] = useState<boolean>(false)
 
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => console.log(codeResponse),
+    onError: (error) => console.log('Login Failed:', error)
+  });
+
   return (
     <Toolbar justify="end">
       <div className="flex flex-row items-center gap-2">
+        <Button
+          leftIcon={<GoogleDriveLogo size={16} weight="bold" />}
+          onClick={login}
+          size="sm"
+          variant="outline"
+          >
+            Connect Google Drive
+        </Button>
         <Button
         leftIcon={<PlusCircle size={16} weight="bold" />}
         onClick={() => setuploadDocumentDialogOpen(true)}

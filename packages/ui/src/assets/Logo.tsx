042c298b3059e8cb5864668a65830eeff5321cf2
Anik Rahman <arahman4710@gmail.com>
2024-12-09 20:25:37 -0500

    Logo color update

--- packages/ui/src/assets/Logo.tsx
+++ packages/ui/src/assets/Logo.tsx
@@ -1,5 +1,5 @@
 export const LogoFull = ({ className }: { className?: string }) => (
-<div className="font-serif text-xl">
+<div className="font-serif text-xl text-amber-500">
   Pagetracker
 </div>
 )

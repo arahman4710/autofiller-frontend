a0887ff3e3b6a944219a29a78a5eb80486a9254e
Anik Rahman <arahman4710@gmail.com>
2025-01-01 19:40:47 -0500

    Remove business name redirect

--- apps/main-app/src/app/(main)/layout.tsx
+++ apps/main-app/src/app/(main)/layout.tsx
@@ -1,4 +1,4 @@
-import { AddBusinessNameRedirect } from '@/components/AddBusinessNameRedirect'
+// import { AddBusinessNameRedirect } from '@/components/AddBusinessNameRedirect'
 import { PageToolbar } from '@/components/PageToolbar'
 import { Sidebar } from '@/components/Sidebar'
 import { MobileHeader } from '@/components/mobile/MobileHeader'
@@ -6,7 +6,7 @@ import { MobileHeader } from '@/components/mobile/MobileHeader'
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-full flex-col md:flex-row">
-      <AddBusinessNameRedirect />
+      {/* <AddBusinessNameRedirect /> */}
       <Sidebar />
       <MobileHeader />
       <div className="border-border bg-background-secondary mt-1 flex w-full flex-col overflow-hidden border-t md:mt-6 md:rounded-tl-xl md:border-l">
